909eeb45fa8da5fa68d7bdf9849c7af3f8539f9a
almoied <31904881+almoied@users.noreply.github.com>
2017-10-24 19:17:36 -0600

    Add files via upload

---
+++ Chad/src/Controler/ChadSub.java
@@ -0,0 +1,12 @@
+package Controler;
+
+public class ChadSub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChadGameApp game = new ChadGameApp();
+		//ChadGameApp g = new ChadGameApp();
+		
+	}
+
+}
